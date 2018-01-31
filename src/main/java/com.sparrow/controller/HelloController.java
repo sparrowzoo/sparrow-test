96d79040f4c98827c96e2cc49e8296c986450961
harry <zh_harry@163.com>
2018-02-01 00:23:02 +0800

    feat:add flash transit

--- src/main/java/com.sparrow/controller/HelloController.java
+++ src/main/java/com.sparrow/controller/HelloController.java
@@ -6,14 +6,18 @@ import com.sparrow.web.support.ViewWithModel;
 
 /**
  * @author harry
- * @date 2018/1/31
  */
 public class HelloController {
     public ViewWithModel hello() throws BusinessException {
-        return ViewWithModel.forward(new HelloVO("我来自遥远的sparrow 星球,累死我了..."));
+        return ViewWithModel.forward("hello", new HelloVO("我来自遥远的sparrow 星球,累死我了..."));
     }
 
     public ViewWithModel fly() throws BusinessException {
-        return ViewWithModel.redirect("hello.jsp", new HelloVO("我是从fly.jsp飞过来，你是不是没感觉？"));
+
+        return ViewWithModel.redirect("fly-here", new HelloVO("我是从fly.jsp飞过来，你是不是没感觉？"));
+    }
+
+    public ViewWithModel transit() throws BusinessException {
+        return ViewWithModel.transit("transit-here", new HelloVO("我从你这歇一会，最终我要到transit-here"));
     }
 }
