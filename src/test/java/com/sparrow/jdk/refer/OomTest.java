74e092917492ae74c5f31e2b59536f56c82a3770
zhanglizhi01 <zhanglizhi01@meicai.cn>
2019-09-17 11:12:01 +0800

    add lettuce redis client

--- src/test/java/com/sparrow/jdk/refer/OomTest.java
+++ src/test/java/com/sparrow/jdk/refer/OomTest.java
@@ -4,15 +4,16 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class OomTest {
+    //-XX:+HeapDumpOnOutOfMemoryError -XX:+HeapDumpBeforeFullGC -XX:+HeapDumpBeforeFullGC -XX:HeapDumpPath=d://data/logs/heap-dump.hprof
     public static void main(String[] args) {
         List<byte[]> byteArray = new ArrayList<>();
         while (true) {
-//            try {
+            try {
                 byte bytes[] = new byte[1024 * 1024 * 1024];
                 byteArray.add(bytes);
-//            } catch (OutOfMemoryError e) {
-//                e.printStackTrace();
-//            }
+            } catch (OutOfMemoryError e) {
+                e.printStackTrace();
+            }
             System.out.println("hi");
         }
     }
