5c72dbacb53c5b09dee5e8059f0bb3fbdb57b535
zhanglizhi01 <zhanglizhi01@meicai.cn>
2019-12-27 14:53:54 +0800

    add trace

--- src/test/java/com/sparrow/jdk/gc/MethodAreaTest.java
+++ src/test/java/com/sparrow/jdk/gc/MethodAreaTest.java
@@ -3,11 +3,12 @@ package com.sparrow.jdk.gc;
 /**
  * @author by harry
  * -Xmx10m -XX:MaxPermSize=9M //<=1.7
- * -Xmx10m -XX:MetaspaceSize=9M //1.8
+ * -Xmx10m -Xms128M -XX:MetaspaceSize=9M //1.8
  */
 public class MethodAreaTest {
-    public static void main(String[] args) {
-        byte[] a = new byte[1024 * 1024 * 5];
+    public static void main(String[] args) throws InterruptedException {
+        //byte[] a = new byte[1024 * 1024 * 500];
         System.out.println("welcome");
+        Thread.sleep(1000000L);
     }
 }
