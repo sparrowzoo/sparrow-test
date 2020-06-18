15b106ef070ad0b9681042316709ad0516dca46a
zhanglizhi01 <zhanglizhi01@meicai.cn>
2020-06-18 19:01:41 +0800

    add trace

--- src/test/java/com/sparrow/jdk/threadlocal/ThreadLocalGCLeak.java
+++ src/test/java/com/sparrow/jdk/threadlocal/ThreadLocalGCLeak.java
@@ -4,17 +4,20 @@ package com.sparrow.jdk.threadlocal;
  * Created by harry on 2018/4/12.
  */
 public class ThreadLocalGCLeak extends Thread {
-   static   ThreadLocal tl = new MyThreadLocal();
+    static ThreadLocal tl = new MyThreadLocal();
+
     public static class MyThreadLocal extends ThreadLocal {
-        private byte[] a = new byte[1024 * 1024 * 1];
+        private byte[] threadObject = new byte[1024 * 1024 * 5];
+
         @Override
         public void finalize() {
             System.out.println(" threadlocal 1 MB finalized.");
         }
     }
 
-    public static class MyBigObject {//占用内存的大对象
-        private byte[] a = new byte[1024 * 1024 * 50];
+    public static class MyBigObject {
+        //占用内存的大对象
+        private byte[] bigObject = new byte[1024 * 1024 * 50];
         @Override
         public void finalize() {
             System.out.println("50 MB Object finalized.");
@@ -25,29 +28,22 @@ public class ThreadLocalGCLeak extends Thread {
         Thread thread = new Thread(new Runnable() {
             @Override
             public void run() {
-
                 tl.set(new MyBigObject());
-                MyBigObject o=(MyBigObject) tl.get();
-
-                tl=null;//手动置为null，让gc回收
+                //tl = null;//手动置为null，让gc回收
                 System.gc();
-                System.out.println(tl);
-                System.out.println("Full GC 1");
-
+                //System.out.println("Full GC 1" + tl.get());
                 //保证线程不死，可以注释掉演示线程死亡的效果
-                while (true){
-                }
+//                while (true) {
+//
+//                }
             }
         });
         thread.setDaemon(false);
         thread.start();
-        System.out.println("Full GC 2");
+
+        Thread.sleep(5000);
         System.gc();
-        Thread.sleep(1000);
-        System.out.println("Full GC 3");
-        System.gc();
-        Thread.sleep(1000);
-        System.out.println("Full GC 4");
-        System.gc();
+        while (true) {
+        }
     }
 }
